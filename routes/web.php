3859c448f3c1635e2c997534f476b0225419a0c6
BrivaHamisi <brivahamisi@gmail.com>
2025-03-03 15:50:50 +0300

    Book Cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/home', [BookController::class, 'index']);  
+Route::get('/shopping-cart', [BookController::class, 'bookCart'])->name('shopping.cart');
+Route::get('/book/{id}', [BookController::class, 'addBooktoCart'])->name('addbook.to.cart');
+Route::patch('/update-shopping-cart', [BookController::class, 'updateCart'])->name('update.sopping.cart');
+Route::delete('/delete-cart-product', [BookController::class, 'deleteProduct'])->name('delete.cart.product');
 
 require __DIR__.'/auth.php';
